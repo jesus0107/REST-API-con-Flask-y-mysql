1a30ac8fb21df58105bdd02d30aa874fdddd1501
Jesus <bep010795@gmail.com>
2022-08-24 22:53:47 -0500

    Obteniendo un curso

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from app_config import settings
 from flask_mysqldb import MySQL
 
@@ -12,14 +12,34 @@ def get_courses():
         sql = "SELECT * FROM api_flask.course;"
         cursor.execute(sql)
         data = cursor.fetchall()
-        print(data)
-        return 'okS'
+        courses = []
+        for row in data:#Accedemos a los datos por medio de su posicion 
+            course = {"id": row[0], "name": row[1], "credits": row[2]}        
+            courses.append(course)
+        print(courses)
+        return jsonify({"message": "Courses listed", "courses": courses})
     except Exception as ex:
         return f'ERROR: {ex}'
 
+@app.route('/courses/<course_code>')
+def get_course(course_code):
+    try:
+        cursor = conn.connection.cursor()
+        sql = "SELECT id, name, credits FROM api_flask.course WHERE id = '{0}'".format(course_code)
+        cursor.execute(sql)
+        data = cursor.fetchone()
+        if data != None:
+            course = {"id": data[0], "name": data[1], "credits": data[2]}
+            return jsonify({"message": "Course found", "course": course})
+        else:
+            return jsonify({"message": "Course not found"})
+    except Exception as ex:
+        return f"Error: {ex}"
+    
+
 @app.errorhandler(404)
 def page_not_found(error):
-    return f"Page not found: {error}"
+    return f"<h1>Page not found</h1> {error}"
 
 
 if __name__ == "__main__":
